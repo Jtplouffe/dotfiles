1f40f33e841fdccde47fe31df6d9058268847646
Jtplouffe <32107801+Jtplouffe@users.noreply.github.com>
2023-03-04 21:45:41 -0500

    Continued LSP setup

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,10 +1,25 @@
 local nvimtree = require("nvim-tree")
+local api = require("nvim-tree.api")
 
 nvimtree.setup({
     sync_root_with_cwd = true,
     respect_buf_cwd = true,
+    disable_netrw = true,
+    hijack_netrw = true,
     update_focused_file = {
         enable = true,
         update_root = true
     }
 })
+
+local function open_nvim_tree(data)
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+        return
+    end
+
+    api.tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
